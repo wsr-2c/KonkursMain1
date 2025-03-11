ffccefac1b4c75d01f0be281f5959b74b2cdc5bf
wsr-2c <wsr-2c@mail.ru>
2025-03-11 16:36:01 +0800

    main

--- app/src/main/java/com/example/myapplication/MainA.java
+++ app/src/main/java/com/example/myapplication/MainA.java
@@ -1,9 +1,6 @@
-package com.example.myapplication.ui.login;
+package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.myapplication.MainA;
-import com.example.myapplication.R;
-
-import java.util.Objects;
-
-public class Splash extends AppCompatActivity {
+public class MainA extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Intent intent = new Intent(Splash.this, MainA.class);
-        startActivity(intent);
-        Handler handler = new Handler();
-        /*public void run();*/
     }
 }
