1323385e4cc691cd0115985ff52608254db3c13b
wsr-2c <wsr-2c@mail.ru>
2025-03-11 16:47:11 +0800

    something

--- app/src/main/java/com/example/myapplication/ui/login/Onboard.java
+++ app/src/main/java/com/example/myapplication/ui/login/Onboard.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplication.MainA;
 import com.example.myapplication.R;
 
 public class Onboard extends AppCompatActivity {
@@ -24,6 +26,14 @@ public class Onboard extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button button = findViewById(R.id.start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Onboard.this, MainA.class);
+                startActivity(intent);
+            }
         });
     }
 }
