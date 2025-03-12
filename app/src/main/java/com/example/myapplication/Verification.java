0a895cbbc67f4cd47c83b50c5b66a788a293a8f0
wsr-2c <wsr-2c@mail.ru>
2025-03-12 15:58:36 +0800

    still work

--- app/src/main/java/com/example/myapplication/Verification.java
+++ app/src/main/java/com/example/myapplication/Verification.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Verification extends AppCompatActivity {
+import com.example.myapplication.ui.login.LoginActivity;
 
+public class Verification extends AppCompatActivity {
+    /// Мясников, 12.03.2025, основной класс
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,15 @@ public class Verification extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        /// переход на экран входа
+        Button button = findViewById(R.id.btnback6);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Verification.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
