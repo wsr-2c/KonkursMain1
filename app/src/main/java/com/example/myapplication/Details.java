367391e76188f0385999b93f555bf9ca6936b389
wsr-2c <wsr-2c@mail.ru>
2025-03-12 10:34:13 +0800

    details

--- app/src/main/java/com/example/myapplication/Details.java
+++ app/src/main/java/com/example/myapplication/Details.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,27 +12,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.myapplication.ui.login.LoginActivity;
-import com.example.myapplication.ui.login.Splash;
+public class Details extends AppCompatActivity {
 
-public class MainA extends AppCompatActivity {
-
-    /// 12.03.2025 Мясников, основной класс
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button button = findViewById(R.id.btnsneak);
+        ImageView imageView = findViewById(R.id.imageView9);
+        Button button = findViewById(R.id.btnback);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainA.this, Details.class);
+                Intent intent = new Intent(Details.this, MainA.class);
                 startActivity(intent);
             }
         });
